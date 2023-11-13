e6841bc9771673f88c4e7b1e0c4627590a156322
Nademtis <Mathias.99.madsen@gmail.com>
2023-11-13 10:33:04 +0100

    Initial commit

---
+++ src/main/java/com/example/deployhttpstest/DeployhttpsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.deployhttpstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployhttpsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployhttpsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deployhttpstest/DeployhttpsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployhttpstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployhttpsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
